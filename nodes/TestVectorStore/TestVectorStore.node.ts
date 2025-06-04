0bf5b3ecbcffd0c28ca6bda577aaf13143632944
Roman Davydchuk <roman.davydchuk@n8n.io>
2025-06-04 21:53:34 +0300

    feat: Add the Supabase vector store; implement 'insert' operation

---
+++ nodes/TestVectorStore/TestVectorStore.node.ts
@@ -0,0 +1,255 @@
+import type { Embeddings } from '@langchain/core/embeddings';
+import type { Document } from '@langchain/core/documents';
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+	ISupplyDataFunctions,
+	NodeExecutionWithMetadata,
+	SupplyData,
+} from 'n8n-workflow';
+import { NodeConnectionType, NodeOperationError } from 'n8n-workflow';
+import { createClient } from '@supabase/supabase-js';
+
+async function populateVectorStore(
+	context: IExecuteFunctions | ISupplyDataFunctions,
+	embeddings: Embeddings,
+	documents: Array<Document<Record<string, unknown>>>,
+	itemIndex: number,
+) {
+	const tableName = context.getNodeParameter('tableName', itemIndex, '', {
+		extractValue: true,
+	}) as string;
+	// TODO: add this property
+	const options = context.getNodeParameter('options', itemIndex, {}) as {
+		queryName: string;
+	};
+	const credentials = await context.getCredentials('testVectorStoreApi');
+	const client = createClient(credentials.host as string, credentials.serviceRole as string);
+
+	try {
+		await SupabaseVectorStore.fromDocuments(documents, embeddings, {
+			client,
+			tableName,
+			queryName: options.queryName ?? 'match_documents',
+		});
+	} catch (error) {
+		if ((error as Error).message === 'Error inserting: undefined 404 Not Found') {
+			throw new NodeOperationError(context.getNode(), `Table ${tableName} not found`, {
+				itemIndex,
+				description: 'Please check that the table exists in your vector store',
+			});
+		} else {
+			throw new NodeOperationError(context.getNode(), error as Error, {
+				itemIndex,
+			});
+		}
+	}
+}
+
+export class TestVectorStore implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Test Vector Store',
+		name: 'testVectorStore',
+		description: 'Test Vector Store Node',
+		icon: 'fa:database',
+		iconColor: 'purple', // for visual distinction
+		group: ['transform'],
+		version: [1, 1.1, 1.2],
+		defaults: {
+			name: 'Test Vector Store',
+		},
+		codex: {
+			categories: ['AI'],
+			subcategories: {
+				AI: ['Vector Stores', 'Tools', 'Root Nodes'],
+				Tools: ['Other Tools'],
+			},
+			resources: {
+				primaryDocumentation: [
+					{
+						url: 'testvectorstore',
+					},
+				],
+			},
+		},
+		credentials: [
+			{
+				name: 'testVectorStoreApi',
+				required: true,
+			},
+		],
+		inputs: `={{
+			((parameters) => {
+				const mode = parameters?.mode;
+				const inputs = [{ displayName: "Embedding", type: "${NodeConnectionType.AiEmbedding}", required: true, maxConnections: 1}]
+
+				if (mode === 'retrieve-as-tool') {
+					return inputs;
+				}
+
+				if (['insert', 'load', 'update'].includes(mode)) {
+					inputs.push({ displayName: "", type: "${NodeConnectionType.Main}"})
+				}
+
+				if (['insert'].includes(mode)) {
+					inputs.push({ displayName: "Document", type: "${NodeConnectionType.AiDocument}", required: true, maxConnections: 1})
+				}
+				return inputs
+			})($parameter)
+		}}`,
+		outputs: `={{
+			((parameters) => {
+				const mode = parameters?.mode ?? 'retrieve';
+
+				if (mode === 'retrieve-as-tool') {
+					return [{ displayName: "Tool", type: "${NodeConnectionType.AiTool}"}]
+				}
+
+				if (mode === 'retrieve') {
+					return [{ displayName: "Vector Store", type: "${NodeConnectionType.AiVectorStore}"}]
+				}
+				return [{ displayName: "", type: "${NodeConnectionType.Main}"}]
+			})($parameter)
+		}}`,
+		properties: [
+			{
+				displayName: 'Operation Mode',
+				name: 'mode',
+				type: 'options',
+				noDataExpression: true,
+				default: 'retrieve',
+				options: [
+					{
+						name: 'Get Many',
+						value: 'load',
+						description: 'Get many ranked documents from vector store for query',
+						action: 'Get ranked documents from vector store',
+					},
+					{
+						name: 'Insert Documents',
+						value: 'insert',
+						description: 'Insert documents into vector store',
+						action: 'Add documents to vector store',
+					},
+					{
+						name: 'Retrieve Documents (As Vector Store for Chain/Tool)',
+						value: 'retrieve',
+						description:
+							'Retrieve documents from vector store to be used as vector store with AI nodes',
+						action: 'Retrieve documents for Chain/Tool as Vector Store',
+						outputConnectionType: NodeConnectionType.AiVectorStore,
+					},
+					{
+						name: 'Update Documents',
+						value: 'update',
+						description: 'Update documents in vector store by ID',
+						action: 'Update vector store documents',
+					},
+					{
+						name: 'Retrieve Documents (As Tool for AI Agent)',
+						value: 'retrieve-as-tool',
+						description: 'Retrieve documents from vector store to be used as tool with AI nodes',
+						action: 'Retrieve documents for AI Agent as Tool',
+						outputConnectionType: NodeConnectionType.AiTool,
+					},
+				],
+			},
+			{
+				displayName: 'Table Name',
+				name: 'tableName',
+				type: 'resourceLocator',
+				default: { mode: 'list', value: '' },
+				required: true,
+				modes: [
+					{
+						displayName: 'From List',
+						name: 'list',
+						type: 'list',
+						typeOptions: {
+							// FIXME:
+							searchListMethod: 'testTableNameSearch',
+						},
+					},
+					{
+						displayName: 'ID',
+						name: 'id',
+						type: 'string',
+					},
+				],
+			},
+		],
+	};
+
+	async execute(
+		this: IExecuteFunctions,
+	): Promise<INodeExecutionData[][] | NodeExecutionWithMetadata[][] | null> {
+		const mode = this.getNodeParameter('mode', 0) as string;
+		// TODO: more operations
+		// probably extract each operation to it's own function at least
+		if (mode !== 'insert') {
+			return [[{ json: {} }]];
+		}
+
+		const embeddings = (await this.getInputConnectionData(
+			NodeConnectionType.AiEmbedding,
+			0,
+		)) as Embeddings;
+		const items = this.getInputData();
+		// TODO: it should be N8nJsonLoader | N8nBinaryLoader | Array<Document<Record<string, unknown>>>
+		// but those types are defined internally and not exposed in the package
+		const documentInput = (await this.getInputConnectionData(
+			NodeConnectionType.AiDocument,
+			0,
+		)) as any;
+		const resultData: INodeExecutionData[] = [];
+		const documentsForEmbedding: Array<Document<Record<string, unknown>>> = [];
+
+		for (let i = 0; i < items.length; i++) {
+			if (this.getExecutionCancelSignal()?.aborted) {
+				break;
+			}
+
+			let processedDocuments: Document<Record<string, unknown>>[] = [];
+			const itemData = items[i];
+			if (!Array.isArray(documentInput)) {
+				// TODO: not type safe, see L140
+				console.log('documentInput is not an array', documentInput);
+				processedDocuments = await documentInput.processItem(itemData, i);
+			} else {
+				console.log('documentInput is an array', documentInput);
+				processedDocuments = documentInput;
+			}
+
+			const serializedDocuments = processedDocuments.map(
+				({ metadata, pageContent }: Document<Record<string, unknown>>) => ({
+					json: { metadata, pageContent },
+					pairedItem: {
+						item: i,
+					},
+				}),
+			);
+
+			resultData.push(...serializedDocuments);
+			documentsForEmbedding.push(...processedDocuments);
+		}
+
+		// TODO: add this property
+		const embeddingBatchSize =
+			(this.getNodeParameter('embeddingBatchSize', 0, 200) as number) ?? 200;
+		for (let i = 0; i < documentsForEmbedding.length; i += embeddingBatchSize) {
+			const nextBatch = documentsForEmbedding.slice(i, i + embeddingBatchSize);
+			await populateVectorStore(this, embeddings, nextBatch, 0);
+		}
+
+		return [resultData];
+	}
+
+	async supplyData(this: ISupplyDataFunctions, _itemIndex: number): Promise<SupplyData> {
+		return {
+			response: {},
+		};
+	}
+}
